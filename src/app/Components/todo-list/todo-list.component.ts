62c41f020d25b0fff55a18fe08b22196ef50a87b
Rupa-421 <rupasrikkd147@gmail.com>
2023-09-07 13:14:50 +0530

    Made smooth transition

--- src/app/Components/todo-list/todo-list.component.ts
+++ src/app/Components/todo-list/todo-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from 'src/app/Services/todo.service';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
+
+ 
+
 })
 export class TodoListComponent implements OnInit {
   incompleteTasks: any[] = [];
